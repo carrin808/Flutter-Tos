7c49f3d463b9779307f349a88de8803af8de86dc
carrin808 <hezekielcarringtone@gmail.com>
2022-09-15 18:35:00 +0300

    Flutter Tos

---
+++ lib/profile_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const int itemCount = 20;
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: itemCount,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text('Item ${(index + 1)}'),
+          leading: const Icon(Icons.person),
+          trailing: const Icon(Icons.select_all),
+          onTap: () {
+            debugPrint('Item ${(index + 1)} selected');
+          },
+        );
+      },
+    );
+  }
+}
